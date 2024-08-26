80d014ae6d57159e614ba8b796d56b2e5279624f
hyunchang <hyunchang52@gmail.com>
2024-08-26 21:18:34 +0900

    pc

---
+++ run.sh
@@ -0,0 +1,25 @@
+#  $1: 생성할 파일 이름
+#  $2: recursive count
+#  $3: memory format
+#!/bin/bash
+
+directory_name=$1
+
+
+if [ -n "$3" ]; then
+	docker rm $(docker ps -qa)
+	docker rmi $(docker image -q)
+	docker system prune -a
+	sudo rm -rf /tmp/*
+	rm -rf ~/.local/share/Trash/files/*
+fi
+
+for ((var=1; var<=$2; var++));
+do
+	./run_all_experiments.py\
+		--model='gpt-4o'\
+		-y ./benchmark-sets/all/libraw.yaml \
+		--work-dir=results/${directory_name}$var
+
+	python3.11 -m report.web -r results/${directory_name}$var -o outputs/${directory_name}$var
+done
